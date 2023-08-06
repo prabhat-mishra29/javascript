c5cab8072c133e4b64c0e0174c17eafba80f134d
Prabhat <prabhatmishra3456@gmail.com>
2023-08-06 11:13:15 +0530

    06.08.2023

--- 13.BOM.js
+++ 13.BOM.js
@@ -20,21 +20,52 @@
     is the same as:
     document.getElementById("header");
 
- Window Size:----------
- Two properties can be used to determine the size of the browser window.
-   a)window.innerHeight - the inner height of the browser window (in pixels)
-   b)window.innerWidth - the inner width of the browser window (in pixels)
+   Window Size:----------
+   Two properties can be used to determine the size of the browser window.
+    a)window.innerHeight - the inner height of the browser window (in pixels)
+    b)window.innerWidth - the inner width of the browser window (in pixels)
 */
 let h=window.innerHeight;
-alert("h : "+h);
 document.write("h : "+h+"<br>");
 let w=window.innerWidth;
-alert("w : "+w);
 document.write("w : "+w);
 /*
-Some other methods:---------------
-    1.window.open() - open a new window
+  Some other methods:---------------["LEARN THESE METHODS IN W3CHOOLS WEBSIRTE"]
+    1.window.open() - open a new window 
     2.window.close() - close the current window
     3.window.moveTo() - move the current window
     4.window.resizeTo() - resize the current window
+*/
+
+/*
+2.JS/Window Screen:---------------
+  >The window.screen object contains information about the user's screen.
+  >The window.screen object can be written without the window prefix.
+  Properties:----
+    1.screen.width:---This property returns the width of the visitor's screen in pixels.
+    2.screen.height;------This property returns the height of the visitor's screen in pixels.
+    3.screen.availWidth:-----This property returns the width of the visitor's screen, in pixels, minus interface    
+                             features like the Windows Taskbar.
+    4.screen.availHeight:---This property returns the height of the visitor's screen, in pixels, minus interface 
+                             features like the Windows Taskbar.
+    5.screen.colorDepth:----This property returns the number of bits used to display one color.
+    6.screen.pixelDepth:---This property returns the pixel depth of the screen.
+*/
+let d1=screen.colorDepth;
+document.write("<br>Screen color Depth: " +d1);
+let d2=screen.pixelDepth;
+document.write("<br>Screen pixel Depth: " +d2);
+let d3=screen.width;
+document.write("<br>Screen width: " +d3);
+let d4=screen.height;
+document.write("<br>Screen height: " +d4);
+let d5=screen.availWidth;
+document.write("<br>Screen available width: " +d5);
+let d6=screen.availHeight;
+document.write("<br>Screen available Height: " +d6);
+
+/*
+3.JS/Window Location:-------------------
+  >The window.location object can be used to get the current page address (URL) and to redirect the browser to a new page.
+  >The window.location object can be written without the window prefix.
 */
