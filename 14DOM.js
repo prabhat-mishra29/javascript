4bdf2f6c3e157c1ea2946f454fe6b4ae58eab73d
Prabhat <prabhatmishra3456@gmail.com>
2023-08-12 13:20:23 +0530

    12.08.2023

---
+++ 14DOM.js
@@ -0,0 +1,19 @@
+/*
+The HTML DOM (Document Object Model) :--------------
+    >When a web page is loaded, the browser creates a Document Object Model of the page.
+    >It defines:--
+        -The HTML elements as objects.
+        -The properties of all HTML elements.
+        -The methods to access all HTML elements.
+        -The events for all HTML elements.
+    >In other words: The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
+    >The HTML DOM model is constructed as a tree of Objects.
+    >There are 3 main type of nodes in the DOM tree.
+        -TEXT node
+        -ELEMENT node
+        -COMMENT node
+     ->Tags are element nodes (or just elements) and form the tree structure.
+     -><html> is at the root, then <head> and <body> are its children, etc. 
+     ->The text inside elements forms text nodes, labelled as #text . A text node contains only a string.
+     ->Text node is a always a leaf node of the tree.
+*/
