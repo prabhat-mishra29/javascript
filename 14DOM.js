55d781b8563e176533ec6e7df429618595493450
Prabhat <prabhatmishra3456@gmail.com>
2023-08-16 18:53:46 +0530

    16.08.2023

--- 14DOM.js
+++ 14DOM.js
@@ -17,3 +17,43 @@ The HTML DOM (Document Object Model) :--------------
      ->The text inside elements forms text nodes, labelled as #text . A text node contains only a string.
      ->Text node is a always a leaf node of the tree.
 */
+// WALKING WITH "DOM" :----------------
+console.log(document.body);  // ---->Returns the <body> element
+console.log(typeof(document.body));
+console.log(document.head);  // ---->Returns the <head> element
+console.log(document.documentElement); // ---->Returns the <html> element or you can say that all elements inside <html> element
+console.log(typeof(document.documentElement));
+/*
+NOTE:--
+    document.body; can sometimes be null if the javascript is written before body tag.
+*/
+
+
+/*
+CHILDREN OF AN ELEMENT :---------
+    >Direct as well as deeply nested elements of an element are called it's children.
+    >child nodes:- Elements that are direct children.
+                   for example - <head> and <body> are children of <html>.
+    >descendant nodes:- All nested elements,children,their childen and so on. 
+*/
+console.log(document.body.firstChild); //First child element
+console.log(document.body.lastChild);  //Last child element
+console.log(document.body.childNodes); //All child nodes
+console.log("Type of document.body.childNodes is :"+typeof(document.body.childNodes));
+    // NOTE:-- A 'space-line' is treated as a #text node!
+console.log(document.body.childNodes[0]); //same as first child element
+console.log(document.body.childNodes[document.body.childNodes.length-1]); //same as last child element
+
+// hasChildNodes() :---
+//    method returns true if the specified node has any child nodes, otherwise false.
+let answer =document.body.hasChildNodes();
+console.log(answer);
+
+/*NOTE:--
+  -ChildNodes looks like an aray but it is not an array.It is a collection.So array methods wont work here.
+  -We can use Array.form(collection) to convert it into an array.
+*/
+let arr=[];
+arr=Array.from(document.body.childNodes);
+console.log(arr);
+console.log("Type of document.body.childNodes is :"+typeof(arr));
