f906f7dc2114a72d8792bdac5d4be54e0faf3676
Prabhat <prabhatmishra3456@gmail.com>
2023-10-17 22:51:42 +0530

    17.10.2023

--- 35.sessionStorage.js
+++ 35.sessionStorage.js
@@ -8,7 +8,7 @@
 let key = prompt("Enter key you want to set")
 let value = prompt("Enter value you want to set")
 sessionStorage.setItem(key, value)//store key/value pair
-console.log(sessionStorage.getItem("name"));//get the value by key 
+console.log(sessionStorage.getItem(key));//get the value by key 
 if (key == "red" || key == "blue") {
     sessionStorage.removeItem("name")//remove the key with it's value
 }
